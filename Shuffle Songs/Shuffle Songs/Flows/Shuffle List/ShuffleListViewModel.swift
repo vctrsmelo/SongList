64799d3efbb8f852cdd58d618df8e249a5dd881f
Victor Melo <victor.contas@outlook.com>
2019-08-31 02:13:55 -0300

    Shuffle algorithm implemented, delegate implemented,  and other ShuffleListViewModel implementations

--- Shuffle Songs/Shuffle Songs/Flows/Shuffle List/ShuffleListViewModel.swift
+++ Shuffle Songs/Shuffle Songs/Flows/Shuffle List/ShuffleListViewModel.swift
@@ -7,19 +7,118 @@
 //
 
 import Foundation
+import UIKit //only imported for UIImage CellData
 
 protocol ShuffleListViewModelDelegate {
-    
+    func didUpdateSongs(viewModel: ShuffleListViewModel)
 }
 
 class ShuffleListViewModel {
     
+    // MARK: - Subelements
+    
+    struct CellData {
+        let title: String
+        let subtitle: String
+        let image: UIImage
+    }
+    
     // MARK: - Properties
+    
+    let artistsIDs = ["909253", "1171421960", "358714030", "1419227", "264111789"]
+    
+    private let limitSongs = 5
+    
+    let service: SongsService
+    var delegate: ShuffleListViewModelDelegate?
+    
+    // all songs received from service
+    private var cacheSongs: [Song]? = []
+
+    // songs to be displayed to user
+    private var shuffledSongs: [Song] = [] {
+        didSet {
+            self.delegate?.didUpdateSongs(viewModel: self)
+        }
+    }
+    
+    var itemLength: Int {
+        return shuffledSongs.count
+    }
     
     // MARK: - Init
     
+    init(service: SongsService) {
+        self.service = service
+
+        getSongsShuffled { maybeSongs in
+            self.shuffledSongs = maybeSongs ?? []
+        }
+    }
+    
     // MARK: - Service
     
+    func getSongsShuffled(completion: @escaping ([Song]?) -> Void) {
+
+        service.fetchSongs(artistsIds: artistsIDs) { [weak self] result in
+            guard let self = self else { return }
+            
+            switch result {
+            case .success(let songs):
+                completion(self.getShuffled(songs, length: self.limitSongs))
+            case .failure(let error):
+                print("[ShuffleListViewModel Error] \(error.localizedDescription)")
+                completion(nil)
+            }
+        }
+    }
+    
     // MARK: - Others
+
+    /**
+     
+     - Returns: Return list with max size of `length` elements, from songs parameter, where for every two songs from same artists, they aren't next each other.
+     - Precondition:
+        - songs.count >= length
+        - length > 0
+     - Postcondition:
+        - return.count <= length
+        - return.count > 0
+     - Important:
+        it is not guaranteed that return will have length elements. The rule of two artists songs should not be next each other is always respected. If it is not possible to return a list of `length` elements, without breaking this rule, an smaller list will be returned.
+    */
+    public func getShuffled(_ songs: [Song], length: Int) -> [Song] {
+        
+        // these guards are related to precondition. Important: return [] in these cases does not break postcondition of "return.count > 0", as precondition are not being respected here.
+        guard songs.count >= length else {
+            assertionFailure("[ShuffleListViewModel] length parameter should be greater than songs.count")
+            return []
+        }
+        
+        guard length > 0 else {
+            assertionFailure("[ShuffleListViewModel] length should not be zero")
+            return []
+        }
+        
+        var inputSongs = songs.shuffled()
+        var outputSongs: [Song] = [inputSongs.first!]
+
+        var i = 1
+        while outputSongs.count < length && i < inputSongs.count {
+            
+            // get next song from different artist.
+            if inputSongs[i].artistName != outputSongs[outputSongs.count-1].artistName {
+                outputSongs.append(inputSongs[i])
+            }
+            
+            i += 1
+        }
+        
+        return outputSongs
+    }
+    
+    func getItem(at index: Int) -> CellData {
+        return CellData(title: shuffledSongs[index].trackName, subtitle: shuffledSongs[index].artistName, image: UIImage())
+    }
     
 }
