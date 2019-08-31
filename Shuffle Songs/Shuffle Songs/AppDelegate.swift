a4c3b5c189483e1997daa33420f7a0a6308d35eb
Victor Melo <victor.contas@outlook.com>
2019-08-31 02:13:14 -0300

    AppDelegate update

--- Shuffle Songs/Shuffle Songs/AppDelegate.swift
+++ Shuffle Songs/Shuffle Songs/AppDelegate.swift
@@ -16,9 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-//        window = UIWindow(frame: UIScreen.main.bounds)
-//        window?.rootViewController = ShuffleListViewController()
-//        window?.makeKeyAndVisible()
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let viewModel = ShuffleListViewModel(service: SongsAPIService())
+        window?.rootViewController = ShuffleListViewController(viewModel: viewModel)
+        
+        window?.makeKeyAndVisible()
 
         return true
     }
