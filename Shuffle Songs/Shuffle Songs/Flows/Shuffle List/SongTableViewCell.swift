036e3e46ccd4a1d1b7b6791f9edfd56dabb8acdd
Victor Melo <victor.contas@outlook.com>
2019-08-31 02:14:56 -0300

    SongTableViewCell implemented

--- Shuffle Songs/Shuffle Songs/Flows/Shuffle List/SongTableViewCell.swift
+++ Shuffle Songs/Shuffle Songs/Flows/Shuffle List/SongTableViewCell.swift
@@ -6,4 +6,16 @@
 //  Copyright © 2019 Victor S Melo. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class SongTableViewCell: UITableViewCell {
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
