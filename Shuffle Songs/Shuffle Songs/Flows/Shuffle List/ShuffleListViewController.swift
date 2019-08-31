091f562e44e9601989abe350497e6c6fa7018c80
Victor Melo <victor.contas@outlook.com>
2019-08-31 02:15:19 -0300

    ShuffleListViewController initial implementation (added TableView, DataSource and delegate)

--- Shuffle Songs/Shuffle Songs/Flows/Shuffle List/ShuffleListViewController.swift
+++ Shuffle Songs/Shuffle Songs/Flows/Shuffle List/ShuffleListViewController.swift
@@ -12,10 +12,71 @@ class ShuffleListViewController: UIViewController {
 
     // MARK: - Properties
     
+    let viewModel: ShuffleListViewModel
+    
+    private let cellID = "songCellID"
+    let tableView: UITableView
+    
+    // MARK: - Init
+    init(viewModel: ShuffleListViewModel) {
+        self.viewModel = viewModel
+        self.tableView = UITableView()
+        
+        super.init(nibName: nil, bundle: nil)
+        
+        viewModel.delegate = self
+    }
+    
+    @available(*, unavailable)
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     // MARK: - View Life Cycle
+    
+    override func viewDidLoad() {
+        self.view.addSubview(tableView)
+        
+        self.tableView.register(SongTableViewCell.self, forCellReuseIdentifier: cellID)
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+
+        tableView.dataSource = self
+        
+    }
     
     // MARK: - Actions
     
     // MARK: - Others
     
+}
+
+extension ShuffleListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.itemLength
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath) as? SongTableViewCell else {
+            fatalError("could not get SongTableViewCell (this should never happens)")
+        }
+        
+        let item = viewModel.getItem(at: indexPath.row)
+        cell.textLabel?.text = item.title
+        cell.detailTextLabel?.text = item.subtitle
+        
+        return cell
+    }
+}
+
+extension ShuffleListViewController: ShuffleListViewModelDelegate {
+    func didUpdateSongs(viewModel: ShuffleListViewModel) {
+        DispatchQueue.main.sync {
+            self.tableView.reloadData()
+        }
+    }
 }
