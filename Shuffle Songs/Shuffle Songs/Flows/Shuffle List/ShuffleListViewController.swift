cfaa7f89313cf1257254d9afc2e3c000fc61549e
Victor Melo <victor.contas@outlook.com>
2019-08-30 22:25:18 -0300

    Adjusted ShuffleListVIewController

---
+++ Shuffle Songs/Shuffle Songs/Flows/Shuffle List/ShuffleListViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ShuffleListViewController.swift
+//  Shuffle Songs
+//
+//  Created by Victor S Melo on 30/08/19.
+//  Copyright © 2019 Victor S Melo. All rights reserved.
+//
+
+import Foundation
