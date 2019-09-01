be1a416d08e030165fdf81fd47c5c612bd791bbc
Victor Melo <victor.contas@outlook.com>
2019-09-01 14:41:38 -0300

    Added APIService, encapsulating request logic

---
+++ Shuffle Songs/Shuffle Songs/Services/APIService.swift
@@ -0,0 +1,34 @@
+//
+//  APIService.swift
+//  Shuffle Songs
+//
+//  Created by Victor S Melo on 31/08/19.
+//  Copyright © 2019 Victor S Melo. All rights reserved.
+//
+
+import Foundation
+
+class APIService {
+
+    func request(_ urlRequest: URLRequest, completion: @escaping (Result<Data, Error>) -> Void) {
+        
+        let urlSessionCompletion: (Data?, URLResponse?, Error?) -> Void = { data, response, error in
+            guard error == nil else {
+                completion(.failure(SongsServiceError.fetchSongsFailure(description: error!.localizedDescription)))
+                return
+            }
+            
+            guard let responseData = data else {
+                completion(.failure(SongsServiceError.fetchSongsFailure(description: "responseData is nil")))
+                return
+            }
+            
+            completion(.success(responseData))
+        }
+        
+        URLSession.shared.dataTask(with: urlRequest, completionHandler: urlSessionCompletion).resume()
+    }
+    
+    
+    
+}
